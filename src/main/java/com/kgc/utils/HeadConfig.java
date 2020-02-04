c5b8370dbb75005bea9d72b40b7539e23ca2a9d4
8t <1004375287@qq.com>
2020-02-04 17:08:28 +0800

    跨域

---
+++ src/main/java/com/kgc/utils/HeadConfig.java
@@ -0,0 +1,15 @@
+package com.kgc.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class HeadConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true) //允许携带cookie
+                .maxAge(3600); //cookie有效时间,单位：秒
+    }
+}
