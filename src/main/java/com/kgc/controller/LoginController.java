82d733650660dfb3a4a891c30417f306c2e46d1c
8t <1004375287@qq.com>
2020-02-06 14:27:15 +0800

    修改为云数据库,添加登录后页面显示用户名

--- src/main/java/com/kgc/controller/LoginController.java
+++ src/main/java/com/kgc/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -21,6 +22,46 @@ public class LoginController {
     @Resource
     private UserService userService;
 
+
+    /**
+     * 退出登录
+     * @return
+     */
+    @GetMapping("/view/logOut")
+    public Result logOut(HttpServletResponse response){
+        Cookie cookie = new Cookie("USER","");
+        cookie.setPath("/");
+        cookie.setMaxAge(1);
+        response.addCookie(cookie);
+        return new Result(null,"已退出",100);
+    }
+
+    /**
+     * 查看Cookie是否有用户
+     * @return
+     */
+    @GetMapping("/view/getUserOfLogin")
+    public Result getUserOfLogin(HttpServletRequest request) throws UnsupportedEncodingException {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length >0) {
+            for (Cookie cookie: cookies) {
+                if("USER".equals(cookie.getName())){
+                    String user =URLDecoder.decode(cookie.getValue(),"UTF-8");
+                    return new Result(user,"Cookie获取成功",100);
+                }
+            }
+        }
+        return new Result(null,"Cookie未获取到",104);
+    }
+
+    /**
+     * 登录
+     * @param username
+     * @param password
+     * @param response
+     * @return
+     * @throws UnsupportedEncodingException
+     */
     @PostMapping("/login")
     public Result login(@RequestParam String username, @RequestParam String password, HttpServletResponse response) throws UnsupportedEncodingException {
         User user = userService.getUserOfLogin(username,password);
